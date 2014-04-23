fb7eae7106780cdfbb6145f8c885651a758a27bd
Don Mamaril <mamarildon@gmail.com>
2014-04-22 21:29:38 -0700

    Bees!!!!!

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  if (this.honeyPot === 0) {
+    return "I ain't got no mo honey to give"
+  } else {
+    this.honeyPot--;
+  }
+}
